170d33045a4759b571fd79b5af25093291d5a4fa
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-13 15:17:04 -0300

    correcoes nos arquivos

--- exercicio/bin/exercicio.dart
+++
@@ -1,5 +0,0 @@
-import 'package:exercicio/exercicio.dart' as exercicio;
-
-void main(List<String> arguments) {
-  print('Hello world: ${exercicio.calculate()}!');
-}
--- exercicio/test/exercicio_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:exercicio/exercicio.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
